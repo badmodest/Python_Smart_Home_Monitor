2f4bf0ff9c9c3bc5c8dff7e9e28916a3411d13df
peace_akame <blackstile2014@gmail.com>
2023-11-22 21:56:49 +0200

    update

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 import paho.mqtt.client as mqtt
+import json
 
 app = Flask(__name__)
 
-# Задаем адрес и порт MQTT брокера
+
 mqtt_broker = "127.0.0.1"
 mqtt_port = 1883
 
-# Создаем словарь для хранения данных датчиков
+
 sensor_data = {
     "Temp": {"value": 0, "unit": "Celsius"},
     "Value": {"value": 0, "unit": "Percentage"},
@@ -17,33 +18,37 @@ sensor_data = {
     "battery": {"value": 0, "unit": "Percent"},
 }
 
-# Функция для обработки сообщений от MQTT брокера
 def on_message(client, userdata, msg):
     try:
-        # Получаем имя датчика из темы
+       
         sensor_name = msg.topic.split("/")[-1]
-        # Обновляем данные датчика
+       
         sensor_data[sensor_name]["value"] = float(msg.payload.decode("utf-8"))
     except Exception as e:
         print(f"Error processing MQTT message: {e}")
 
-# Настраиваем подключение к MQTT брокеру
+
 mqtt_client = mqtt.Client()
 mqtt_client.on_message = on_message
 mqtt_client.connect(mqtt_broker, mqtt_port, 60)
 
-# Подписываемся на темы датчиков
+
 for sensor in sensor_data:
     mqtt_topic = f"topic/{sensor}"
     mqtt_client.subscribe(mqtt_topic)
 
-# Запускаем цикл обработки сообщений MQTT
+
 mqtt_client.loop_start()
 
-# Определение маршрута для главной страницы
+
 @app.route("/")
 def index():
     return render_template("index.html", sensor_data=sensor_data)
+
+
+@app.route("/data")
+def get_data():
+    return json.dumps(sensor_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ emulate_sens.py
@@ -0,0 +1,21 @@
+import paho.mqtt.publish as publish
+import time
+import random
+
+mqtt_broker = "127.0.0.1"
+mqtt_port = 1883
+
+sensor_topics = ["topic/Temp", "topic/Value", "topic/Pressure", "topic/sensor4", "topic/Battery", "topic/battery"]
+
+def emulate_sensors():
+    while True:
+        for topic in sensor_topics:
+           
+            sensor_value = round(random.uniform(0, 100), 2)
+            
+            publish.single(topic, payload=str(sensor_value), hostname=mqtt_broker, port=mqtt_port)
+            print(f"Published value {sensor_value} to topic {topic}")
+        time.sleep(10) 
+
+if __name__ == "__main__":
+    emulate_sensors()
