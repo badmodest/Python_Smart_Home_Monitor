13d2743746a93a6ec27d553c1564da916e28c472
peace_akame <76851151+badmodest@users.noreply.github.com>
2023-11-27 20:06:18 +0200

    Update emulate_sens.py

--- emulate_sens.py
+++ emulate_sens.py
@@ -5,17 +5,20 @@ import random
 mqtt_broker = "127.0.0.1"
 mqtt_port = 1883
 
-sensor_topics = ["topic/Temp", "topic/Value", "topic/Pressure", "topic/sensor4", "topic/Battery", "topic/battery"]
+temp_topics = ["topic/Temp", "topic/Battery"]
+sensor_topics = ["topic/Value", "topic/Pressure", "topic/sensor4", "topic/battery"]
 
 def emulate_sensors():
     while True:
         for topic in sensor_topics:
-           
-            sensor_value = round(random.uniform(0, 100), 2)
-            
+                    sensor_value = round(random.uniform(0, 100), 2)
+        for topc in temp_topics:
+sensor_value = round(random.uniform(2,5),2)
+
+ 
             publish.single(topic, payload=str(sensor_value), hostname=mqtt_broker, port=mqtt_port)
-            print(f"Published value {sensor_value} to topic {topic}")
-        time.sleep(10) 
+            print(f"Value sended: {sensor_value} to topic: {topic}")
+        time.sleep(100) 
 
 if __name__ == "__main__":
     emulate_sensors()
